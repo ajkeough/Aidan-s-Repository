d442d44b6f03dd660769b19c1329966a259d078b
ajkeough <78373741+ajkeough@users.noreply.github.com>
2021-04-15 22:08:45 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,17 @@
+import subprocess
+import os
+#Origin of the dataset
+#https://www.kaggle.com/gpreda/covid-world-vaccination-progress
+
+os.chdir('/Users/aidansstuff/Downloads')
+
+
+import pandas as pd
+df = pd.read_csv(r'/Users/aidansstuff/Downloads/country_vaccinations.csv')
+df = df.drop(columns=['source_website', 'source_name', 'iso_code'], axis=1)
+
+df.info()
+
+#change the file type from .CSV to .TSV
+csv.writer(open('country_vaccinations.tsv', 'w+'), delimiter='\t').writerows(csv.reader(open("country_vaccinations.csv")))
+
